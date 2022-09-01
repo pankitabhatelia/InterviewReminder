dd0794ef4f65bf9ecb7065c8f4ad7d317e1de506
pankitabhatelia <pankitakb@infusionanalysts.com>
2022-09-01 18:26:48 +0530

    InterviewReminder App with added spinner from firebase initial commit

--- app/src/main/java/activitiy/AddInterview.kt
+++ app/src/main/java/activitiy/AddInterview.kt
@@ -1,31 +1,35 @@
 package activitiy
 
 import Interfaces.LoadFirebase
+import android.R
 import android.app.DatePickerDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.interviewreminderapp.databinding.ActivityAddReminderBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
+import com.google.firebase.firestore.FirebaseFirestore
 import model.Department
 import viewmodel.AddInterviewViewModel
 import java.util.*
-import kotlin.collections.ArrayList
 
-class AddInterview : AppCompatActivity(), LoadFirebase {
-    private lateinit var databaseReference: DatabaseReference
+
+class AddInterview : AppCompatActivity() {
     private lateinit var firebaseUSer: FirebaseUser
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityAddReminderBinding
     private lateinit var viewModel: AddInterviewViewModel
-    private lateinit var loadFirebase: LoadFirebase
     private val cal = Calendar.getInstance()
     private lateinit var datePickerDialog: DatePickerDialog
+    private lateinit var databaseReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddReminderBinding.inflate(layoutInflater)
@@ -44,25 +48,38 @@ class AddInterview : AppCompatActivity(), LoadFirebase {
         binding.etInterviewDate.setOnClickListener {
             datePickerDialog.show()
         }
-
-        databaseReference = FirebaseDatabase.getInstance().getReference("Department")
-        loadFirebase = this
-        databaseReference.addValueEventListener(object : ValueEventListener {
-            var departmentList: MutableList<Department> = ArrayList<Department>()
-            override fun onDataChange(snapshot: DataSnapshot) {
-                for (departmentSnapshot in snapshot.children) {
-                    departmentList.add(departmentSnapshot.getValue<Department>(Department::class.java)!!)
-                    loadFirebase.onFirebaseLoadSuccess(departmentList)
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                loadFirebase.onFirebaseLoadFailed(error.message)
-            }
-        })
+        getData()
         observer()
     }
 
+    private fun getData() {
+        var departmentList = ArrayList<String>()
+        lateinit var firestore: FirebaseFirestore
+        firestore = FirebaseFirestore.getInstance()
+        firestore.collection("Department")
+            .get()
+            .addOnSuccessListener {
+                val department1 = it.documents.get(0).data?.get("departmentName").toString()
+                val department2 = it.documents.get(1).data?.get("departmentName").toString()
+                val department3 = it.documents.get(2).data?.get("departmentName").toString()
+                val department4 = it.documents.get(3).data?.get("departmentName").toString()
+                val department5 = it.documents.get(4).data?.get("departmentName").toString()
+                val department6 = it.documents.get(5).data?.get("departmentName").toString()
+                departmentList.add(department1)
+                departmentList.add(department2)
+                departmentList.add(department3)
+                departmentList.add(department4)
+                departmentList.add(department5)
+                departmentList.add(department6)
+                val adapter = ArrayAdapter(
+                    this@AddInterview,
+                    R.layout.simple_spinner_item,
+                    departmentList
+                )
+                adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+                binding.spinnerDepartment.adapter = adapter
+            }
+    }
 
     private val dateSelectedListener =
         DatePickerDialog.OnDateSetListener { _, myear, mmonth, mdayOfMonth ->
@@ -83,43 +100,5 @@ class AddInterview : AppCompatActivity(), LoadFirebase {
             }
         }
     }
-
-    override fun onFirebaseLoadSuccess(departmentList: List<Department>) {
-        val departmentName = getdepartmentList(departmentList)
-        val adapter =
-            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, departmentName)
-        binding.spinnerDepartment.adapter = adapter
-    }
-
-    override fun onFirebaseLoadFailed(message: String) {
-
-    }
-
-    private fun getdepartmentList(departmentList: List<Department>): List<String> {
-        val result = ArrayList<String>()
-        for (department in departmentList) {
-            result.add(department.departmentName!!)
-        }
-        return result
-    }
-  /*  fDatabaseRoot.child("areas").addValueEventListener(new ValueEventListener() {
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-            for (DataSnapshot areaSnapshot: dataSnapshot.getChildren()) {
-            String areaName = areaSnapshot.child("areaName").getValue(String.class);
-
-            Spinner areaSpinner = (Spinner) findViewById(R.id.spinner);
-            final String[] areas = {areaName};
-            ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(UAdminActivity.this, android.R.layout.simple_spinner_item, areas);
-            areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            areaSpinner.setAdapter(areasAdapter);
-        }
-        }
-
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
-
-        }
-    });*/
 
 }
